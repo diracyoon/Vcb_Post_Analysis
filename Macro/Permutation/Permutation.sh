672502f806e8633c9e544f323c6b6e84655e2ae5
Inseok Yoon <diracyoon@gmail.com>
2022-09-06 14:50:02 +0900

    Macros for Permutation

---
+++ Macro/Permutation/Permutation.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+if [ $1 -gt 11 ]; then
+    echo "Out of range"
+    exit
+fi
+
+n_jet=`expr $1 % 4 + 4`
+pre_cut=`expr $1 / 4`
+final_kin=`expr $1 / 8` 
+
+if [ $final_kin -eq 1 ]; then
+    pre_cut=0
+fi
+
+echo $n_jet $pre_cut $final_kin
+
+dir="/data6/Users/isyoon/Vcb_Post_Analysis/Macro/Permutation"
+
+${dir}/Permutation.py -e 2018 -ch Mu -njet ${n_jet} -pre_cut $pre_cut -final_kin $final_kin
+
+#ls dataset/weights/TMVAClassification*
+
+if [ $pre_cut -eq 0 ] && [ $final_kin -eq 0 ]
+then 
+    mkdir -p ${dir}/PrePermutation_${n_jet}Jets/weights/
+    
+    mv dataset/weights/TMVAClassification_* ${dir}/PrePermutation_${n_jet}Jets/weights/
+    mv Vcb_PrePermutation_TTLJ_WtoCB_${n_jet}Jets.root ${dir}/PrePermutation_${n_jet}Jets
+    #mv job_$1.??? ${dir}/PrePermutation_${n_jet}Jets
+
+elif [ $pre_cut -eq 1 ]; then
+    mkdir -p ${dir}/PreCut_${n_jet}Jets/weights/
+
+    mv dataset/weights/TMVAClassification_* ${dir}/PreCut_${n_jet}Jets/weights/
+    mv Vcb_PreCut_TTLJ_WtoCB_${n_jet}Jets.root ${dir}/PreCut_${n_jet}Jets
+    #mv job_$1.??? ${dir}/PreCut_${n_jet}Jets
+
+elif [ $final_kin -eq 1 ]; then
+    mkdir -p ${dir}/Permutation_${n_jet}Jets/weights/
+ 
+    mv dataset/weights/TMVAClassification_* ${dir}/Permutation_${n_jet}Jets/weights/
+    mv Vcb_Permutation_TTLJ_WtoCB_${n_jet}Jets.root ${dir}/Permutation_${n_jet}Jets
+    #mv job_$1.??? ${dir}/Permutation_${n_jet}Jets
+
+fi
